742ab8939cac869f96b598249b71cf7765f571da
Dmitriy Emelyanov <harnblaze@gmail.com>
2023-01-21 21:06:51 +0300

    hook useRef

--- src/app/components/examples/useRef/renderCount.jsx
+++ src/app/components/examples/useRef/renderCount.jsx
@@ -1,12 +1,24 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
+import Divider from "../../common/divider";
 import SmallTitle from "../../common/typografy/smallTitle";
 const RenderCountExample = () => {
-    const someRef = useRef("someRef");
+    const renderCount = useRef(0);
+    const [otherState, setOtherState] = useState(false);
+    const toggleOtherState = () => {
+        setOtherState(!otherState);
+    };
+    useEffect(() => {
+        renderCount.current++;
+    });
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количества рендеров</SmallTitle>
-            {someRef.current}
+            <Divider />
+            <p>render count:{renderCount.current}</p>
+            <button className="btn btn-primary" onClick={toggleOtherState}>
+                Toggle other state
+            </button>
         </CardWrapper>
     );
 };
